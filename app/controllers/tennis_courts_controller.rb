fa52c658ee8bb4d6b7be5a57e6f51cb0202fd4cb
tanguydamois <tdamois@gmail.com>
2020-08-24 16:51:52 -0400

    tennis_court controller CRUD

--- app/controllers/tennis_courts_controller.rb
+++ app/controllers/tennis_courts_controller.rb
@@ -1,2 +1,45 @@
 class TennisCourtsController < ApplicationController
+  def index
+    @tennis_courts = Tennis_court.all
+  end
+
+  def new
+    @tennis_court = Tennis_court.new
+  end
+
+  def create
+    @tennis_court = Tennis_court.new(tennis_court_params)
+    @tennis_court.save
+
+    redirect_to tennis_court_path(@tennis_court)
+
+  end
+
+  def show
+    @tennis_court = Tennis_court.find(params[:id])
+  end
+
+   def edit
+    @tennis_court = Tennis_court.find(params[:id])
+  end
+
+  def update
+    @tennis_court = Tennis_court.find(params[:id])
+    @tennis_court.update(tennis_court_params)
+
+    redirect_to tennis_court_path(@tennis_court)
+  end
+
+
+  def destroy
+    @tennis_court = Tennis_court.find(params[:id])
+    @tennis_court.destroy
+
+    redirect_to tennis_court_path
+  end
+  private
+
+  def tennis_court_params
+    params.require(:tennis_court).permit(:name, :address, :price_per_hour, :detail)
+  end
 end
