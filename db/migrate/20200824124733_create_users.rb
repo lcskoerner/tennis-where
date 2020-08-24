bf320af1202c340de5bf50f88d36af97a7d829b9
tanguydamois <tdamois@gmail.com>
2020-08-24 08:55:53 -0400

    create user model and controller

---
+++ db/migrate/20200824124733_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :role
+      t.string :first_name
+      t.string :last_name
+      t.text :address
+      t.integer :phone_number
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_24_124733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "role"
+    t.string "first_name"
+    t.string "last_name"
+    t.text "address"
+    t.integer "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
