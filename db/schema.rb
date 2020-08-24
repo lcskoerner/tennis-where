0dd28abaea9f3c85bc65ab6f3d46f65830f9bc9a
tanguydamois <tdamois@gmail.com>
2020-08-24 16:21:43 -0400

    create Booking model with foreigh key

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_201424) do
+ActiveRecord::Schema.define(version: 2020_08_24_202004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "date"
+    t.integer "start_hour"
+    t.integer "end_hour"
+    t.integer "number_of_hour"
+    t.bigint "user_id", null: false
+    t.bigint "tennis_court_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tennis_court_id"], name: "index_bookings_on_tennis_court_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
@@ -46,6 +59,8 @@ ActiveRecord::Schema.define(version: 2020_08_24_201424) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "bookings", "tennis_courts"
+  add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "tennis_courts"
   add_foreign_key "reviews", "users"
   add_foreign_key "tennis_courts", "users"
