84e5a2843ac1cf7f5130a7bf08f734f533a11d2b
Lucas Koerner <lcskoerner@gmail.com>
2020-08-25 19:08:34 -0400

    user and tennis_court validations

--- app/models/tennis_court.rb
+++ app/models/tennis_court.rb
@@ -1,3 +1,4 @@
 class TennisCourt < ApplicationRecord
   belongs_to :user
+  validates :name, :address, :price_per_hour, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tennis_courts
+  validates :first_name, :last_name, :phone_number, presence: true
 end
