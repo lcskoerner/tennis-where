09bc0c6129744d1ebe431141bc4d843a7635948a
Lucas Koerner <lcskoerner@gmail.com>
2020-08-28 17:34:48 -0400

    search by date bookings index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,9 @@
 class BookingsController < ApplicationController
+  def index
+    if params[:query].present?
+      @bookings = Booking.where(date: params[:date])
+    else
+      @bookings = Booking.all
+    end
+  end
 end
