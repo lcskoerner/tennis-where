62af625726f1340485662974b11605d6caea648e
Lucas Koerner <lcskoerner@gmail.com>
2020-08-28 17:40:53 -0400

    rails g migrate ChangeDateType to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_004810) do
+ActiveRecord::Schema.define(version: 2020_08_28_213905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_08_28_004810) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.integer "date"
+    t.string "date"
     t.integer "start_hour"
     t.integer "end_hour"
     t.integer "number_of_hour"
