33c8f7246b191033195f03de6a8bd20d2f7cc3f0
Lucas Koerner <lcskoerner@gmail.com>
2020-08-28 19:21:33 -0400

    slot picker component

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,13 @@
 class BookingsController < ApplicationController
   def index
     if params[:query].present?
-      @bookings = Booking.where(date: params[:date])
+      @bookings = Booking.where(date: params[:query])
     else
       @bookings = Booking.all
     end
+
+    all_hours = (8..20).to_a
+    occ_hours = @bookings.map(&:start_hour)
+    @slots = all_hours - occ_hours
   end
 end
