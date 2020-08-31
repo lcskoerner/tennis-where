cc5d5bdd70e8bea3f821ac6ad120f083b5b45c5d
Lucas Koerner <lcskoerner@gmail.com>
2020-08-30 21:24:29 -0400

    geocoded address

--- app/models/tennis_court.rb
+++ app/models/tennis_court.rb
@@ -2,4 +2,6 @@ class TennisCourt < ApplicationRecord
   belongs_to :user
   validates :name, :address, :price_per_hour, presence: true
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
